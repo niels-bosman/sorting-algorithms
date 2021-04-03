468101aa9ab78d26a737dc32dbdabd11690ad715
nehlis <nielsbosman@mail.com>
2021-04-03 16:48:05 +0200

    Formatting

--- sorting/InsertionSortStrategy.ts
+++ sorting/InsertionSortStrategy.ts
@@ -1,6 +1,6 @@
-import {SortStrategy} from "./SortStrategy";
-import {Sorter} from "./Sorter";
-import {Timer} from "../timer/Timer";
+import {SortStrategy} from './SortStrategy'
+import {Sorter} from './Sorter'
+import {Timer} from '../timer/Timer'
 
 export class InsertionSortStrategy extends Sorter implements SortStrategy
 {
@@ -14,17 +14,17 @@ export class InsertionSortStrategy extends Sorter implements SortStrategy
     }
 
     public sort(): number[] {
-        let length = this.numbers.length;
+        let length = this.numbers.length
 
         for (let i = 1; i < length; i++) {
-            const current = this.numbers[i];
-            let previous = i - 1;
+            const current = this.numbers[i]
+            let previous = i - 1
             while (previous >= 0 && this.numbers[previous] > current) {
-                this.numbers[previous + 1] = this.numbers[previous];
-                previous = previous - 1;
+                this.numbers[previous + 1] = this.numbers[previous]
+                previous = previous - 1
             }
-            this.numbers[previous + 1] = current;
+            this.numbers[previous + 1] = current
         }
-        return this.numbers;
+        return this.numbers
     }
 }
