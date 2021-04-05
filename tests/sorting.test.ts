c51d6373cb664d6a31f06a014dba0de00a500efd
nehlis <nielsbosman@mail.com>
2021-04-05 14:24:46 +0200

    naming

--- tests/sorting.test.ts
+++ tests/sorting.test.ts
@@ -6,7 +6,7 @@ import {QuickSortStrategy} from '../sorting/QuickSortStrategy'
 const sortable: number[] = [2, 3, 5, 8, 1, 4, 10, 9, 6, 7]
 const sortedRight: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-describe('sort', () => {
+describe('sorting', () => {
     it('bubble-sort', async () => {
         const bubble = new BubbleSortStrategy()
         expect(await bubble.sort(sortable)).toStrictEqual(sortedRight)
